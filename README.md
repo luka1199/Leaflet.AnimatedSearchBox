3fa1548c3c34333c4ca6acec10d36c33a1b83a00
Luka Steinbach <luka.steinbach@gmx.de>
2020-03-27 15:10:53 +0100

    Added demo gifs and implemented expand left and right

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # Leaflet.AnimatedSearchbox
 ![Leaflet 1.0 compatible!](https://img.shields.io/badge/Leaflet%201.0-%E2%9C%93-1EB300.svg?style=flat)  
 
-An animated search box plugin for Leaflet.
+Provides a simple and animated search box plugin for Leaflet.
+
+![demo](assets/demo_500px.gif)
+
+## Usage 
+
+### Examples
 
 [Check out the demo](https://luka1199.github.io/Leaflet.AnimatedSearchbox/examples)
 
-## Usage 
+### Requirements
+
+- [Google Icons](https://google.github.io/material-design-icons/)
 
 ## Options
--- src/Leaflet.AnimatedSearchBox.js
+++ src/Leaflet.AnimatedSearchBox.js
@@ -16,68 +16,41 @@
 }(function (L) {
     L.Control.searchbox = L.Control.extend({
         options: {
-            id: '',
             class: '',
             position: 'topright',
+            expand: 'left',
             collapsed: true
         },
-
+        
         onAdd: function (map) {
-            this._container = L.DomUtil.create('div', 'leaflet-control leaflet-searchbar-container');
-            if (this.options.class != '') {
-                L.DomUtil.addClass(this._container, this.options.class);
-            }
+            this._create();
+            
             this._collapsed = this.options.collapsed;
-
-            this._input = L.DomUtil.create('input', 'leaflet-searchbar', this._container);
-            this._input.id = this.options.id;
-            this._input.setAttribute('type', 'text');
-            this._input.style.width = this.options.width;
-            this._input.style.height = this.options.height;
-
-            this._button = L.DomUtil.create('button', 'leaflet-searchbar-button', this._container);
-            this._button.setAttribute('type', 'button');
-            this._button.style.width = this.options.height;
-            this._button.style.height = this.options.height;
-            this._icon = L.DomUtil.create('i', 'fa fa-search', this._button);
-
             if (this.options.collapsed) {
                 this.hide();
             }
-
+            
             L.DomEvent.disableClickPropagation(this._container);
-
+            
             L.DomEvent.on(this._button, "click", this._onClick, this)
-
+            
             return this._container;
         },
 
         onRemove: function (map) {
-
+            
         },
-
+        
+        
         getValue: function () {
             return this._input.value
         },
 
-        onInput: function (event, handler) {
-            L.DomEvent.on(this._input, event, handler, this);
+        setValue: function (value) {
+            this._input.value = value;
+            return this
         },
-
-        offInput: function (event, handler) {
-            L.DomEvent.off(this._input, event, handler, this);
-        },
-
-        onButton: function (event, handler) {
-            var wrapper = this._buttonHandlerWrapper(handler);
-            L.DomEvent.on(this._button, event, wrapper, this);
-        },
-
-        offButton: function (event, handler) {
-            var wrapper = this._buttonHandlerWrapper(handler);
-            L.DomEvent.off(this._button, event, wrapper, this);
-        },
-
+        
         hide: function () {
             L.DomUtil.addClass(this._container, "collapsed");
             this._input.blur();
@@ -85,36 +58,111 @@
             setTimeout(() => {
                 this._collapsed = true;
             }, 600);
+            
+            return this;
         },
-
+        
         show: function () {
             L.DomUtil.removeClass(this._container, "collapsed");
             setTimeout(() => {
                 this._collapsed = false;
             }, 600);
+            
+            return this;
         },
-
+        
         toggle: function () {
             if (L.DomUtil.hasClass(this._container, "collapsed")) {
                 this.show();
             } else {
                 this.hide();
             }
+            
+            return this;
+        },
+        
+        clear: function () {
+            this._input.value = '';
+            
+            return this;
         },
 
+        onInput: function (event, handler) {
+            L.DomEvent.on(this._input, event, handler, this);
+
+            return this
+        },
+
+        offInput: function (event, handler) {
+            L.DomEvent.off(this._input, event, handler, this);
+
+            return this
+        },
+
+        onButton: function (event, handler) {
+            var wrapper = this._buttonHandlerWrapper(handler);
+            L.DomEvent.on(this._button, event, wrapper, this);
+
+            return this
+        },
+
+        offButton: function (event, handler) {
+            var wrapper = this._buttonHandlerWrapper(handler);
+            L.DomEvent.off(this._button, event, wrapper, this);
+
+            return this
+        },
+        
         _onClick: function () {
             if (this._collapsed) {
                 this.show();
                 this._input.focus();
             }
         },
-
+        
         _buttonHandlerWrapper: function (handler) {
             return function () {
                 if (!this._collapsed) {
                     handler();
                 }
             }
+        },
+        
+        _create: function () {
+            this._container = L.DomUtil.create('div', 'leaflet-control leaflet-searchbox-container');
+            if (this.options.class != '') {
+                L.DomUtil.addClass(this._container, this.options.class);
+            }
+
+            if (this.options.expand == 'left') {
+                this._createInput('left');
+                this._createButton('right');
+            } else if (this.options.expand == 'right') {
+                this._createButton('left');
+                this._createInput('right');
+            }
+        },
+
+        _createInput: function (position) {
+            this._input = L.DomUtil.create(
+                'input', 
+                'leaflet-searchbox leaflet-searchbox-' + position, 
+                this._container);
+            this._input.setAttribute('type', 'text');
+            this._input.style.width = this.options.width;
+            this._input.style.height = this.options.height;
+        },
+
+        _createButton: function (position) {
+            this._button = L.DomUtil.create(
+                'button', 
+                'leaflet-searchbox-button leaflet-searchbox-button-' + position, 
+                this._container);
+            this._button.setAttribute('type', 'button');
+            this._button.style.width = this.options.height;
+            this._button.style.height = this.options.height;
+            this._icon = L.DomUtil.create('i', 'material-icons', this._button);
+            this._icon.innerHTML = 'search';
         }
     });
 
